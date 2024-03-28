ec5c4a87abda7c956f43ed5be25a18d80d20665c
rfsyhb <rafisyihab3@gmail.com>
2024-03-28 09:33:48 +0700

    proteksi fitur kontak dengan conditional authedUser

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,24 +1,52 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Navigation from "./Navigation";
 import AddPage from "../pages/AddPage";
 import HomePageWrapper from "../pages/HomePage";
 
-function ContactApp() {
-  return (
-    <div className="contact-app">
-      <header className="contact-app__header">
-        <h1>Aplikasi Kontak</h1>
-        <Navigation />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePageWrapper />} />
-          <Route path="/add" element={<AddPage />} />
-        </Routes>
-      </main>
-    </div>
-  );
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authedUser: null,
+    };
+  }
+
+  render() {
+    if (this.state.authedUser === null) {
+      return (
+        <div className="contact-app">
+          <header className="contact-app__header">
+            <h1>Aplikasi Kontak</h1>
+            {/* <Navigation /> */}
+          </header>
+          <main>
+            <Routes>
+              <Route path="/*" element={<p>Halaman Login</p>} />
+              <Route path="/register" element={<p>Halaman Register</p>} />
+              {/* <Route path="*" element={<p>Error 404</p>} /> */}
+            </Routes>
+          </main>
+        </div>
+      );
+    }
+
+    return (
+      <div className="contact-app">
+        <header className="contact-app__header">
+          <h1>Aplikasi Kontak</h1>
+          <Navigation />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePageWrapper />} />
+            <Route path="/add" element={<AddPage />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default ContactApp;
